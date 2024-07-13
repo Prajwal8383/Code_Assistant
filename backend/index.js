6eaf63ee82a05c8cdf0969f4c49da611bc3478a7
Rayan-Ey <rayanahmed1805@gmail.com>
2024-07-13 19:43:11 +0530

    Rayan/backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,83 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+const prettier = require('prettier');
+const { ESLint } = require('eslint');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Access your API key as an environment variable (see "Set up your API key" above)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+app.use(cors());
+var genAI;
+app.use(bodyParser.json());
+app.listen(5000, () => {
+    genAI = new GoogleGenerativeAI("AIzaSyDtUetUHqMG5mgHqHwbfh9t9sSsLWe0wX8");
+    
+  console.log('Server running on port 5000');
+});
+
+
+
+app.post('/complete', async (req, res) => {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+  const { code } = req.body;
+  
+  try {
+    const prompt=`complete the code : ${code}"`;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    res.json({ text });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/explain', async (req, res) => {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+  const { code } = req.body;
+  try{
+    const prompt=`explain the code : ${code}"`;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    res.json({ text });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/beautify', async (req, res) => {
+  const { code, language } = req.body;
+  console.log(code,language);
+  try {
+    let formattedCode;
+    if (language === 'javascript') {
+      formattedCode = await prettier.format(code, { parser: 'babel' });
+    } else if (language === 'python') {
+      formattedCode = await prettier.format(code, { parser: 'python' });
+    } else {
+      throw new Error('Unsupported language');
+    }
+    console.log(formattedCode);
+    res.json({ beautifiedCode: formattedCode });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// app.post('/debug', async (req, res) => {
+//   const { code, language } = req.body;
+//   try {
+//     const eslint = new ESLint();
+//     const results = await eslint.lintText(code);
+//     console.log(results);
+//     res.json({ debugResults: results });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+
